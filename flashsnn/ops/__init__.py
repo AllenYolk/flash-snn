2bd8cc12341871298585f143b105137147562576
AllenYolk <allen.yfhuang@gmail.com>
2026-01-12 18:07:36 +0800

    fix: fix a wrong import

--- flashsnn/ops/__init__.py
+++ flashsnn/ops/__init__.py
@@ -3,7 +3,6 @@ from .plif import *
 from .psn import *
 from .li import *
 from .pli import *
-from .spike_function import *
 from .surrogate_kernels import *
 from .ssa import *
 from .qka import *
